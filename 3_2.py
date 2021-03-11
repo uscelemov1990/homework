ca85fb29e9d5c053681e85da0a1cded18d8bbff7
aleksey <uscelemov1990@gmail.com>
2021-03-11 19:26:41 +0300

    added lection-4

--- 3_2.py
+++
@@ -1,8 +0,0 @@
-'''2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя:
-имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры
-как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.'''
-
-def user(name, surname, year, city, email, phone):
-    return name, surname, year, city, email, phone
-
-print(user(name='Ivan', surname='Ivanov', year=1990, city='Tambov', email='user@mail.com', phone=777))
